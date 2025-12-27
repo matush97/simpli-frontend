51771be007684907b68418f349552e1863daa4ce
Matus Hudak <hudym97@gmail.com>
2025-12-27 23:19:54 +0100

    primereact library use for DataTable view

--- src/announcement/pages/AnnouncementsPage.jsx
+++ src/announcement/pages/AnnouncementsPage.jsx
@@ -1,12 +1,18 @@
 import {useEffect, useState} from "react";
+import {useNavigate} from 'react-router-dom';
+
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import {Button} from 'primereact/button';
+
 import {listAnnouncements} from "../../services/announcement";
-import AnnouncementList from "../components/AnnouncementList";
 
 function AnnouncementsPage() {
     const [announcements, setAnnouncements] = useState([]);
     const [search, setSearch] = useState('');
     const [category, setCategory] = useState('');
     const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     async function loadData() {
         setLoading(true)
@@ -24,11 +30,30 @@ function AnnouncementsPage() {
         loadData();
     }, [search, category])
 
+    console.log("announcements", announcements);
+
+    function actionTemplate(rowData) {
+        return (
+            <Button
+                icon="pi pi-pencil"
+                rounded
+                text
+                onClick={() => navigate(`/announcement/${rowData._id}`)}
+            />
+        );
+    }
+
     return (
         <div>
             <h1>Announcements</h1>
 
-            {loading ? <p>Loading...</p> : <AnnouncementList announcements={announcements}/>}
+            <DataTable value={announcements} paginator rows={10} loading={loading}>
+                <Column field="title" header="Title"/>
+                <Column field="publicationDate" header="Publication date"/>
+                <Column field="lastUpdate" header="Last update"/>
+                <Column field="category" header="Categories"/>
+                <Column body={actionTemplate} header="Actions"/>
+            </DataTable>
         </div>
     )
 }
